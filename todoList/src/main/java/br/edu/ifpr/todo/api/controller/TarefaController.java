43962752cc7d4f1402e0e389be93d0efdf7bae91
Arthur <ian.arthur.yw@gmail.com>
2025-09-29 15:09:32 -0300

    alt

--- todoList/src/main/java/br/edu/ifpr/todo/api/controller/TarefaController.java
+++ todoList/src/main/java/br/edu/ifpr/todo/api/controller/TarefaController.java
@@ -1,7 +1,12 @@
 package br.edu.ifpr.todo.api.controller;
 
+import br.edu.ifpr.todo.api.dto.TarefaRequest;
+import br.edu.ifpr.todo.api.dto.TarefaResponse;
+import br.edu.ifpr.todo.domain.model.Tarefa;
+import br.edu.ifpr.todo.domain.model.TodoStatus;
+import br.edu.ifpr.todo.domain.service.TarefaService;
+import jakarta.validation.Valid;
 import java.util.List;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,17 +14,10 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 
-import br.edu.ifpr.todo.api.dto.TarefaRequest;
-import br.edu.ifpr.todo.api.dto.TarefaResponse;
-import br.edu.ifpr.todo.domain.model.Tarefa;
-import br.edu.ifpr.todo.domain.model.TodoStatus;
-import br.edu.ifpr.todo.domain.service.TarefaService;
-import jakarta.validation.Valid;
+
 
 @RestController
 @RequestMapping("/api/tarefas")
@@ -39,16 +37,9 @@ public class TarefaController {
     }
 
     @GetMapping
-    public List<TarefaResponse> listar(
-            @RequestParam(required = false) TodoStatus status,
-            @RequestParam(required = false) Boolean importante) {
-
+    public List<TarefaResponse> listar(@RequestParam(required = false) TodoStatus status, @RequestParam(required = false) Boolean importante) {
         List<Tarefa> tarefas = service.listar(null, status, importante, null);
-
-        List<TarefaResponse> resposta = tarefas.stream()
-                                               .map(this::toResponse)
-                                               .toList();
-
+        List<TarefaResponse> resposta = tarefas.stream().map(this::toResponse).toList();
         return resposta;
     }
 
@@ -59,9 +50,7 @@ public class TarefaController {
     }
 
     @PatchMapping("/{id}")
-    public TarefaResponse atualizarParcial(@PathVariable Long id,
-                                           @Valid @RequestBody TarefaRequest dto) {
-
+    public TarefaResponse atualizarParcial(@PathVariable Long id, @Valid @RequestBody TarefaRequest dto) {
         Tarefa tarefa = service.atualizarParcial(id, dto);
         return toResponse(tarefa);
     }
@@ -74,13 +63,13 @@ public class TarefaController {
 
     private TarefaResponse toResponse(Tarefa tarefa) {
         return new TarefaResponse(
-                tarefa.getId(),
-                tarefa.getNome(),
-                tarefa.getDescricao(),
-                tarefa.getStatus(),
-                tarefa.getDataCriacao(),
-                tarefa.getDataEntrega(),
-                tarefa.getImportante()
+            tarefa.getId(),
+            tarefa.getNome(),
+            tarefa.getDescricao(),
+            tarefa.getStatus(),
+            tarefa.getDataCriacao(),
+            tarefa.getDataEntrega(),
+            tarefa.getImportante()
         );
     }
 }
